f650676ecbb61918f830036c7e8c8ca18dfe6faa
AL FAYOUMI BILAL <bilal.al-fayoumi@etu.u-bordeaux.fr>
2026-03-11 23:57:13 +0100

    Fix admin entry route and add error boundary

--- JDC/src/App.jsx
+++ JDC/src/App.jsx
@@ -5,12 +5,14 @@ import VehicleDetail from './Pages/VehicleDetail'
 import TradeIn from './Pages/TradeIn'
 import Contact from './Pages/Contact'
 import Administrative from './Pages/Administrative'
+import AdminLanding from './Pages/AdminLanding'
 import AdminVehicles from './Pages/AdminVehicles'
 import AdminCarteGrise from './Pages/AdminCarteGrise'
 import MentionsLegales from './Pages/MentionsLegales'
 import CGV from './Pages/CGV'
 import PolitiqueConfidentialite from './Pages/PolitiqueConfidentialite'
 import Layout from './Components/Layout'
+import AdminErrorBoundary from './Components/AdminErrorBoundary'
 import ScrollToTop from './Components/ScrollToTop'
 import { Toaster } from './ui/sonner'
 
@@ -26,9 +28,9 @@ function App() {
           <Route path="/trade-in" element={<TradeIn />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/administrative" element={<Administrative />} />
-          <Route path="/admin" element={<Navigate to="/admin/vehicles" replace />} />
-          <Route path="/admin/vehicles" element={<AdminVehicles />} />
-          <Route path="/admin/carte-grise" element={<AdminCarteGrise />} />
+          <Route path="/admin" element={<AdminErrorBoundary><AdminLanding /></AdminErrorBoundary>} />
+          <Route path="/admin/vehicles" element={<AdminErrorBoundary><AdminVehicles /></AdminErrorBoundary>} />
+          <Route path="/admin/carte-grise" element={<AdminErrorBoundary><AdminCarteGrise /></AdminErrorBoundary>} />
           <Route path="/mentions-legales" element={<MentionsLegales />} />
           <Route path="/cgv" element={<CGV />} />
           <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
